8541452f8277d483ca5b67990c862f7e00e0920b
Brainiac <brainiacerudite@gmail.com>
2024-12-30 14:35:31 +0100

    add wrapper fn to controller and handles error catching

--- src/controllers/example.controller.js
+++ src/controllers/example.controller.js
@@ -1,13 +1,11 @@
 const HttpException = require("../exceptions/HttpException");
+const asyncHandler = require("../utils/asyncHandler");
 
-const example = async (req, res, next) => {
-  try {
-    // Some logic that might throw an error
-    throw new HttpException(400, "This is a bad request");
-  } catch (error) {
-    next(error);
-  }
-};
+const example = asyncHandler(async (req, res, next) => {
+  // Some logic that might throw an error
+  throw new HttpException(400, "This is a bad request");
+  // res.status(200).json({ success: true, message: "This is an example route" });
+});
 
 const exampleController = { example };
 module.exports = exampleController;
